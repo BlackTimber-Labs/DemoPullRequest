924e6482534cadcd4b912f44508aa9117daf9632
mohdadnan786 <60983364+mohdadnan786@users.noreply.github.com>
2021-10-01 21:04:13 +0530

    Add files via upload

--- C C++/main.cpp
+++ C C++/main.cpp
@@ -1,34 +1,18 @@
 #include <iostream>
+
 using namespace std;
-class number{
-    int number1;
-    int number2;
-public:
-    number(){}
-    number(int num1 , int num2,int temp);
-    void show();
-};
-number::number (int num1 ,int num2,int temp)
-{
-    number1=num1;
-    number2=num2;
-    temp=num1;
-    num1=num2;
-    num2=temp;
-};
-void number::show(){
-    cout<<"the number after swaping are a is "<<number1<<" and b is "<<number2;
-}
+
 
 int main(){
-    number n1,n2;
-    int num1;
-    int num2;
-    cout<<"Enter the value of a and b"<<endl;
-    cin>>num1>>num2;
-    n1=number(num1,num2);
-    n1.show();
-    
+int i,x;
+
+    cout<<"Enter any number:";
+
+    cin>>x;
+
+    for(i=0;i<=10;i++)
+
+    cout<<"\n"<<x<<" * "<<i<<" = "<<x*i;
+
     return 0;
-    
 }
