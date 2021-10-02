96b4747f18f67a216d7d01817e09bc3bf431d0ff
meheer-shukla <3meheer@gmail.com>
2021-10-02 16:30:59 +0530

    Update sakshamsomani2345.cpp

--- C C++/sakshamsomani2345.cpp
+++ C C++/sakshamsomani2345.cpp
@@ -1,22 +1,38 @@
-#include <algorithm>
-#include <iterator>
 #include <iostream>
- template<typename ForwardIterator> void selection_sort(ForwardIterator begin,
-                                                       ForwardIterator end) {
-  for(auto i = begin; i != end; ++i) {
-    std::iter_swap(i, std::min_element(i, end));
-  }
-}
- 
-int main() {
- int a[] = {125, 0, 695, 3, -256, -5, 214, 44, 55};
-  std::cout << "Original numbers:\n";
-  copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << "\n";
-  selection_sort(std::begin(a), std::end(a));
- std::cout << "Sorted numbers:\n";
-  copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << "\n";
-    return 0;
+#include <string>
+using namespace std;
 
+string change_letter(string str) {
+
+	int char_code;
+	
+	for (int x = 0; x < str.length(); x++)
+	{
+		char_code = int(str[x]);
+
+		if (char_code == 122)
+		{
+			str[x] = char(97);
+		}
+		else if (char_code == 90)
+		{
+			str[x] = char(65);
+		}
+		else if (char_code >= 65 && char_code <= 90 || char_code >= 97 && char_code <= 122)
+		{
+			str[x] = char(char_code + 1);
+		}
+	
+	}
+
+	return str;
+}
+
+int main() 
+{
+	cout << "Original string: w3resource"; 
+	cout << "\nNew string: " << change_letter("w3resource");
+	cout << "\n\nOriginal string: Python"; 
+	cout << "\nNew string: " << change_letter("Python");
+	return 0;
 }
