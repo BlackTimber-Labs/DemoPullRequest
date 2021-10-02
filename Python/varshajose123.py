fb87b8c159a4d22b95e8ce6cfa35890846be5a21
varshajose123 <90182342+varshajose123@users.noreply.github.com>
2021-10-02 19:52:23 +0530

    Create varshajose123.py

---
+++ Python/varshajose123.py
@@ -0,0 +1,11 @@
+num = int(input("enter number:"))
+if num > 1:
+    for i in range(2, int(num/2)+1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+ 
+else:
+    print(num, "is not a prime number")
