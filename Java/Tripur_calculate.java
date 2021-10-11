83f10da860de1b8cc8a798adc9e145f81fff0645
tripur3121 <78254006+tripur3121@users.noreply.github.com>
2021-10-11 10:32:26 +0530

    Update Tripur_calculate.java

--- Java/Tripur_calculate.java
+++ Java/Tripur_calculate.java
@@ -4,16 +4,14 @@ public class ArrayAverage {
  
     public static void main(String[] args) {
  
-        // create an array
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
  
-        // getting array length
+       
         int length = array.length;
  
-        // default sium value.
+     
         int sum = 0;
  
-        // sum of all values in array using for loop
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
