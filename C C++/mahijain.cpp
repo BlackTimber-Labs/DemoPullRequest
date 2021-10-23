7047df14aa2166f4452d8df66b5e0ced9a88c90b
Mahi Jain <mahijain.mj80@gmail.com>
2021-10-23 14:08:19 +0530

    commit to main

--- C C++/mahijain.cpp
+++ C C++/mahijain.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 void print_Nto1(int n)
 {
-    if( n==0) return;
+    if(n==0) return;
     cout<<n<<" ";
     print_Nto1(n-1);
 }
 int main()
 {
-    int a=100
-    ;
+    int a=100;
     print_Nto1(a);
     return 0;
 }
