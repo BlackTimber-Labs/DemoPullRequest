b200d96b9f540d74c2827f82615f724d943b67e8
Sejzz <91632881+Sejzz@users.noreply.github.com>
2021-10-25 11:51:16 +0530

    Create sejzz_loops.java

---
+++ sejzz_loops.java
@@ -0,0 +1,12 @@
+public class loop {
+
+	public static void main(String[] args) {
+		
+	
+		for(int i=10; i>=0; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Happy new year!");
+		
+	}
+}
