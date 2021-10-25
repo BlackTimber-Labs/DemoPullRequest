229e0497d46e8bea0aef4bc1b9edfe1781d4ee8d
Sejzz <91632881+Sejzz@users.noreply.github.com>
2021-10-25 11:53:02 +0530

    Create sejzz_switch.java

---
+++ sejzz_switch.java
@@ -0,0 +1,29 @@
+public class switch_case {
+    
+
+        public static void main(String[] args) {
+        
+         String day = "Friday";
+         
+         switch(day) {
+          case "Sunday": System.out.println("It is Sunday!");
+          break;
+          case "Monday": System.out.println("It is Monday!");
+          break;
+          case "Tuesday": System.out.println("It is Tuesday!");
+          break;
+          case "Wednesday": System.out.println("It is Wednesday!");
+          break;
+          case "Thursday": System.out.println("It is Thursday!");
+          break;
+          case "Friday": System.out.println("It is Friday!");
+          break;
+          case "Saturday": System.out.println("It is Saturday!");
+          break;
+          default: System.out.println("That is not a day!");
+         }
+           
+        }
+       
+    
+}
